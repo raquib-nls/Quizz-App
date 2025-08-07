2849f89e6af7a4d0022cef55f9c14ec6faf2b796
Mohd Raquib Hussain <raquibhussain108@gmail.com>
2025-08-07 21:07:34 +0530

    poty of app.py

--- app.py
+++ app.py
@@ -483,6 +483,6 @@ def import_questions():
     return result
 
 if __name__=="__main__":
-    # port = int(os.environ.get("PORT",5000))
-    # app.run(host="0.0.0.0",port=port,debug=False)
-    app.run(debug=True)
+    port = int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0",port=port,debug=False)
+    
